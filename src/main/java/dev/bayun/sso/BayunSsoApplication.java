ef7a42e8a5903314fbce9b740fdd4a52cce8daef
Maksim Yaskov <ms.yaskov@gmail.com>
2023-10-02 17:43:46 +0300

    Initial commit

---
+++ src/main/java/dev/bayun/sso/BayunSsoApplication.java
@@ -0,0 +1,13 @@
+package dev.bayun.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BayunSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BayunSsoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/bayun/sso/BayunSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bayun.sso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BayunSsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
